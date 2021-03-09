c64f88f222d34130eeb538f9d4215ec97089079a
Jardel Lins <jardel.a7x@gmail.com>
2021-03-09 20:51:26 -0300

    projeto move it concluído

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../context/ChallengesContext';
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
+    return(
+        <div className={styles.profileContainer}>
+            <img src="https://avatars.githubusercontent.com/u/44972197?s=460&v=4" alt="Jarde Felipe"/>
+            <div>
+                <strong>Jardel Felipe</strong>
+                <p>
+                    <img src="icons/level.svg" alt="level"/>
+                    Level {level}
+                </p>
+            </div>
+        </div>
+    )
+}
